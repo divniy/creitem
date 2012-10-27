65740bfdd0aa51920c73fac97c08bc375aae517e
Dmitriy Paramonov <dev@divniy.com>
2012-10-27 15:42:24 +0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Creitem::Application.config.secret_token = 'a028cb2ca48bfc6ace5a7e2f6b1ac2a4fcf53444541486883b4b64b3d494bb8c1cd6c4c7221ce48b9e821f2095a3a8bf31ad7b6d84d74c7f51d13884a306949d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Creitem::Application.config.session_store :cookie_store, key: '_creitem_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Creitem::Application.config.session_store :active_record_store
