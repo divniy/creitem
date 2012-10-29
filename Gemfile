994156050d98e7ccb4a76016de03b38d145ced33
Dmitriy Paramonov <dev@divniy.com>
2012-10-29 04:14:06 +0400

    Add product image

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ end
 
 gem 'jquery-rails'
 gem 'simple_form'
+gem 'carrierwave'
+gem "mini_magick"
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ActiveRecord::Base
-  attr_accessible :title, :description, :price
+  attr_accessible :title, :description, :price, :image, :image_cache
 
   validates :title, :description, :price, presence: true
   validates_uniqueness_of :title
   validates_numericality_of :price, greater_than_or_equal_to: 0.01
+
+  mount_uploader :image, ImageUploader
 end
